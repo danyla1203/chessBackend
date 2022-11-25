1c6ddd77bfad5e6a45f08f3caea360d2c4ca30b1
Daniil <zelenko.danil@gmail.com>
2022-11-26 01:48:13 +0200

    extend base error

--- src/errors/Game/NotFound.ts
+++ src/errors/Game/NotFound.ts
@@ -1,6 +1,7 @@
+import { BaseError } from '..';
 import { GameErrors } from './types';
 
-export class GameNotFound {
+export class GameNotFound extends BaseError {
   type = GameErrors.GAME_NOT_FOUND;
   message = 'Game not found';
 }
