74854c8c40da5bbefd3c6afc1a1c4df20645e7f2
Daniil <zelenko.danil@gmail.com>
2022-11-26 21:29:55 +0200

    extends base err

--- src/errors/Game/InactiveGame.ts
+++ src/errors/Game/InactiveGame.ts
@@ -1,6 +1,7 @@
+import { BaseError } from '..';
 import { GameErrors } from './types';
 
-export class InactiveGameError {
+export class InactiveGameError extends BaseError {
   type = GameErrors.GAME_IS_INACTIVE;
   message = 'Game is inative';
 }
